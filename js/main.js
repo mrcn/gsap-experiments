6f228c58038fdf093178e00f2901b5864aa9dbd8
mrcn <makepeopleup@gmail.com>
2020-10-16 13:49:13 -0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,49 @@
 function init(){
+  
+    const projects = document.querySelectorAll('.project'); 
+  
+    projects.forEach((project) => {
+      
+      gsap.from(project, {
+        opacity: 0,
+        yPercent: 5,
+        scrollTrigger: {
+          trigger: project.querySelector('img'),
+          start: 'top bottom-=300',
+          end: 'top center',
+          markers: true,
+        }
+      })
+      
+    });
     
-}
-
-window.addEventListener('load', function(){
-    init();
-});
+    
+  //     gsap.from("#project02", {
+  //       scrollTrigger: {
+  //         trigger:"#project02",
+  //         start: 'top bottom-=50',
+  //         end: 'bottom bottom-=50',
+  //         // scrub: 1,
+  //         // markers: true
+  //     },
+  //       duration: 3,
+  //       x: -500,
+  //     })   
+    
+  //     gsap.from("#project01", {
+  //       scrollTrigger: {
+  //         trigger:"#project01",
+  //         start: 'top bottom-=50',
+  //         end: 'bottom bottom-=50',
+  //         // scrub: 1,
+  //         markers: true
+  //     },
+  //       duration: 3,
+  //       x: 500,
+  //     })   
+  }
+  
+  window.addEventListener('load', function(){
+      init();
+  });
+  
