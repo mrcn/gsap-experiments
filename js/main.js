0834e4f5f29392111ae00a719aee0e4c3d9f78c2
mrcn <makepeopleup@gmail.com>
2020-10-19 11:44:09 -0500

    fix code

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
 function init(){
   
-let project = document.querySelector(".project");
 
-    const projects = document.querySelectorAll('.project'); 
-  
-    projects.forEach((project) => {
-      
-      gsap.from(project, {
-        opacity: 0,
-        yPercent: 5,
-        duration:  3,
-        scrollTrigger: {
-          trigger: project.querySelector('img'),
-          start: 'top bottom-=300',
-          end: 'top center',
-          markers: true,
-          scrub: true
-        }
-      })
-      
-    });
+const projects = document.querySelectorAll('.project'); 
+
+projects.forEach((project) => {
     
+    gsap.from(project, {
+    opacity: 0,
+    x: -500,
+    duration:  3,
+    scrollTrigger: { 
+        trigger: project.querySelector('img'),
+        start: 'top bottom-=300',
+        end: 'top center',
+        markers: true
+    }
+    })
     
-  }
-  
-  window.addEventListener('load', function(){
-      init();
-  });
-  
+});
+}
+
+window.addEventListener('load', function(){
+    init();
+});
