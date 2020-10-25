630bd90b3aea81828bc27747f9116634dba44d19
Marcin Dabrowski <marcin@Marcins-MacBook-Pro.local>
2020-10-25 08:11:58 -0500

    remove gsap include

--- slider-01/js/main.js
+++ slider-01/js/main.js
@@ -1,11 +1,8 @@
-const { default: gsap } = require("gsap/gsap-core");
-const { getDisplayName } = require("next/dist/next-server/lib/utils");
-
 function init(){
 
     
     // welcome to the projects gallery
-    // gsap.set('.projects', {autoAlpha: 1});
+    gsap.set('.projects', {autoAlpha: 1});
     gsap.set('.project', {x:'-100%'});
     
 }
