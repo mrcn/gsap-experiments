79c80ffd74f7a66b1850ab41cd71b0286ae4e786
Marcin Dabrowski <marcin@Marcins-MacBook-Pro.local>
2020-10-25 16:49:40 -0500

    wrap variables in functions

--- slider-01/js/main.js
+++ slider-01/js/main.js
@@ -5,34 +5,56 @@ function init(){
     gsap.set('.projects', {autoAlpha: 1}); // this makes the arrows visible, which are in the "projects" div
     gsap.set('.project', {x:'-100%'}); // this moves a project off screen
     
-    const element = document.querySelector('div.project03');
-        projectClasses = element.className.split(' '); // 2. by splitting the body class with a space
-        projectClass = projectClasses[1]; // 3. taking the second value of the class
+    createTimelineIn(2);
 
     // https://ihatetomatoes.net/module-1/g3ps-animation-in-604/
-    const tlIn = gsap.timeline();
-    tlIn.fromTo(element, {
-        autoAlpha: 0,
-        x: '-100%'
-    }, {
-        duration: .7,
-        x: 0,
-        autoAlpha: 1,
-        onStart: updateClass, // 3. which we pass to the onStart callback function 
-        onStartParams: [projectClass] // 4. and then we update the class
-    });
 
-    const tlOut = gsap.timeline();
-    tlOut.to(element, {
-        duration: 0.7, 
-        x: 250, 
-        autoAlpha: 0
-    });
+    function createTimelineIn(slideIndex){
 
-    const tlTransition = gsap.timeline();
-    tlTransition
-        .add(tlIn)
-        .add(tlOut, '+=2') 
+        const element = document.querySelector('div.project0'+slideIndex);
+        projectClasses = element.className.split(' '); // 2. by splitting the body class with a space
+        projectClass = projectClasses[1]; // 3. taking the second value of the class
+        
+        const tlIn = gsap.timeline();
+        tlIn.fromTo(element, {
+            autoAlpha: 0,
+            x: '-100%'
+        }, {
+            duration: .7,
+            x: 0,
+            autoAlpha: 1,
+            onStart: updateClass, // 3. which we pass to the onStart callback function 
+            onStartParams: [projectClass] // 4. and then we update the class
+        });
+        
+        return tlIn;
+
+    }
+
+    function createTimelineOut(slideIndex){
+
+        const tlOut = gsap.timeline();
+        tlOut.to(element, {
+            duration: 0.7, 
+            x: 250, 
+            autoAlpha: 0
+        });
+
+        return tlOut;
+    }
+
+
+    function transition(){
+        const tlTransition = gsap.timeline();
+        tlTransition
+            .add(tlIn)
+            .add(tlOut, '+=2') ;
+
+        return tlTransition;
+
+    }
+
+
 
     // 1. removing the body class 
     function updateClass(projectClass){
